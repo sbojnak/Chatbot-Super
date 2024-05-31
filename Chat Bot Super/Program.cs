fc526e1bb0234c48d798124b994bf361d14275a3
Stefan Bojnak <stefan.bojnak@ideastatica.com>
2024-05-31 13:58:55 +0200

    Added code from last session

---
+++ Chat Bot Super/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+Chat();
+
+
+void Chat()
+{
+    Console.WriteLine("What do you want?");
+    string input;
+    do
+    {
+        input = Console.ReadLine();
+    } while (string.IsNullOrEmpty(input));
+
+    var theme = Topics.RecognizeTheme(input);
+
+    if (theme != null)
+    {
+        switch (theme)
+        {
+            case SupportedTopic.Movies:
+                Console.WriteLine(Topics.GetTopTenMovies());
+                break;
+            case SupportedTopic.Weather:
+                Console.WriteLine(Topics.GetDayFromQuestion(input));
+                break;
+            case SupportedTopic.NumberSort:
+                Console.WriteLine(Topics.ExtractAndSortIntegers(input));
+                break;
+            case SupportedTopic.DayInWeek:
+                Console.WriteLine(Topics.TalkDayInWeek(input));
+                break;
+        }
+        Chat();
+    }
+    else
+    {
+        Console.WriteLine("Could not recognize theme");
+        Chat();
+    }
+}
