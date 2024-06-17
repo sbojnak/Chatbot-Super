7cb34244c0d839cb788daaed525ad3485662d9c4
vstekk <jakub.racek@gmail.com>
2024-06-17 15:42:50 +0200

    ADD class documentation

--- Chat Bot Super/AppOutput/ConsoleOutputHandler.cs
+++ Chat Bot Super/AppOutput/ConsoleOutputHandler.cs
@@ -1,5 +1,12 @@
-﻿public class ConsoleOutputHandler : IOutputHandler
+﻿/// <summary>
+/// Chatbot class for handling output in Console environment.
+/// </summary>
+public class ConsoleOutputHandler : IOutputHandler
 {
+    /// <summary>
+    /// Write text as a Line in a Console.
+    /// </summary>
+    /// <param name="text">Text to print.</param>
     public void WriteLine(string text)
     {
         Console.WriteLine(text);
