7483edc222d7c4940353141599bb31ae9f629998
Andrea Habáňová <andrea@jetveo.io>
2024-06-17 15:16:29 +0200

    weather and movies classes

---
+++ Chat Bot Super/Topics/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chat_Bot_Super.Topics
+{
+    public class Movies : ITopic
+    {
+        public string GetAnswer(string input)
+        {
+            return "Here's a list of the top ten movies of all time:\n" +
+                "1. The Godfather (1972)\n" +
+                "2. The Shawshank Redemption (1994)\n" +
+                "3. Schindler's List (1993)\n" +
+                "4. Raging Bull (1980)\n" +
+                "5. Casablanca (1942)\n" +
+                "6. Citizen Kane (1941)\n" +
+                "7. Gone with the Wind (1939)\n" +
+                "8. The Wizard of Oz (1939)\n" +
+                "9. One Flew Over the Cuckoo's Nest (1975)\n" +
+                "10. Lawrence of Arabia (1962)";
+        }
+    }
+}
---
+++ Chat Bot Super/Topics/Weather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chat_Bot_Super.Topics
+{
+    public class Weather : ITopic
+    {
+        private string GetWeatherYesterday()
+        {
+            return "Yesterday's weather was sunny with a high of 75°F and a low of 55°F.";
+        }
+        private string GetWeatherToday()
+        {
+            return "Today's weather is partly cloudy with a high of 78°F and a low of 60°F.";
+        }
+        private string GetWeatherTomorrow()
+        {
+            return "Tomorrow's weather will be rainy with a high of 70°F and a low of 50°F.";
+        }
+
+        public string GetAnswer(string input)
+        {
+            if (input.ToLower().Contains("yesterday"))
+            {
+                return GetWeatherYesterday();
+            }
+            else if (input.ToLower().Contains("today"))
+            {
+                return GetWeatherToday();
+            }
+            else if (input.ToLower().Contains("tomorrow"))
+            {
+                return GetWeatherTomorrow();
+            }
+            else
+            {
+                return "I don't understand your question about weather, please try asking again.";
+            }
+        }
+    }
+}
